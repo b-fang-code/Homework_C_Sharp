c02667b523a877e213408a58732b3743c1685fa4
b_fang <shampuk@gmail.com>
2022-12-09 12:03:19 +0300

    add Home Work 008

---
+++ HomeWork008_C#/Program.cs
@@ -0,0 +1,233 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+/*
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = FillArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine("==============================================");
+SortArray(array);
+PrintArray(array);
+
+//////////////////////// Методы ////////////////////////////////
+
+int[,] FillArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+*/
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+/*
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = FillArray(rows, columns, 0, 10);
+PrintArray(array);
+SumRow(array);
+
+//////////////////////// Методы ////////////////////////////////
+
+int[,] FillArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void SumRow(int[,] array)
+{
+    int sumMinRow = 0;  /// - минимальная сумма чисел в строке
+    int sumRow = 0;   /// - сумма чисел в строке (другой)
+    int numbMinRow = 0; /// - номер строки с минимальной суммой
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+
+        sumMinRow += array[0,j];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            sumRow += array[i, j];
+        }
+        if (sumRow < sumMinRow)
+        {
+            sumMinRow = sumRow;
+            numbMinRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"Номер строки с наименьшей суммой чисел в ней = {numbMinRow +1}");
+}
+*/
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+/*
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array1 = FillArray(rows, columns, 0, 10);
+Console.WriteLine("Первая матрица:");
+PrintArray(array1);
+Console.WriteLine("==============================================");
+int[,] array2 = FillArray(rows, columns, 0, 10);
+Console.WriteLine("Вторая матрица:");
+PrintArray(array2);
+Console.WriteLine("==============================================");
+
+if (array1.GetLength(0) != array2.GetLength(1))
+{
+    Console.WriteLine("Матрицы нельзя перемножить!");
+    return;
+}
+else
+{
+    int[,] array3 = ResulArray(rows, columns, array1, array2);
+    Console.WriteLine("Результат умножения:");
+    PrintArray(array3);
+}
+
+//////////////////////// Методы ////////////////////////////////
+
+int[,] FillArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] ResulArray(int rows, int columns, int[,] array1, int[,] array2)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                result[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return result;
+}
+*/
+
+
